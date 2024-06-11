0a066d1bc9910ed8124333d74ee6f12d378668f4
pudvik <prudviksanikala54@gmail.com>
2024-06-11 12:00:46 +0530

    terraform

--- cicd/main.tf
+++ cicd/main.tf
@@ -5,7 +5,7 @@ module "jenkins" {
 
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-09ebad62096ad78b0"] #replace your SG
-  subnet_id = "subnet-08b9c3233899c830b" #replace your Subnet
+  subnet_id = "subnet-0ee5e97fb770a7bdc" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -18,10 +18,10 @@ module "jenkins_agent" {
 
   name = "jenkins-agent"
 
-  instance_type          = "t3.small"
+  instance_type          = "t2.medium"
   vpc_security_group_ids = ["sg-09ebad62096ad78b0"]
   # convert StringList to list and get first element
-  subnet_id = "subnet-08b9c3233899c830b"
+  subnet_id = "subnet-0ee5e97fb770a7bdc"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
@@ -29,16 +29,31 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("/c/devops-78s/devops-78s.pub")
+  # ~ means windows home directory
+}
+
 module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "nexus"
 
-  instance_type          = "t3.small"
+  instance_type          = "t2.medium"
   vpc_security_group_ids = ["sg-09ebad62096ad78b0"]
   # convert StringList to list and get first element
-  subnet_id = "subnet-08b9c3233899c830b"
+  subnet_id = "subnet-0ee5e97fb770a7bdc"
   ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
   tags = {
     Name = "nexus"
   }
